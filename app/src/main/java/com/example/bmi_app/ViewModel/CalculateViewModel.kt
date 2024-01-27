f5a1878c59eeb1fad812758500f773d775ce9e23
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-01-27 21:52:07 +0530

    View Model

--- app/src/main/java/com/example/bmi_app/ViewModel/CalculateViewModel.kt
+++ app/src/main/java/com/example/bmi_app/ViewModel/CalculateViewModel.kt
@@ -9,10 +9,11 @@ class CalculateViewModel: ViewModel() {
     private var _result = MutableLiveData(0.0)
     val result: LiveData<Double> = _result
 
-    fun calculateBodyMassIndex(h: Int, w: Int){
+    fun calculateBodyMassIndex(h: Int, w: Int): Double{
         val heightCm: Double = h.toDouble()/100.0
         var r: Double = (w/(heightCm*heightCm))
         r = String.format("%.1f",result).toDouble()
         _result.value = r
+        return _result.value!!
     }
 }
